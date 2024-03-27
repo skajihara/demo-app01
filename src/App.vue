0652189b0fdc770d179ecbb996a51c295fef3328
skajihara <t15cs015@gmail.com>
2024-03-27 16:02:30 +0900

    section7-2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-
+  <main>
+    <h1>今日何食べる？</h1>
+  </main>
 </template>
 
 <style scoped>
-
+main {
+  width: 350px;
+  margin: 100px auto;
+  text-align: center;
+}
 </style>
